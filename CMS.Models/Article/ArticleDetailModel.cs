02245aed992d5e6b3d82e24b62185fcfd4e1232a
VaclavKosak <vaclav.kosak@gmail.com>
2021-02-09 11:56:21 +0100

    Article - user part

--- CMS.Models/Article/ArticleDetailModel.cs
+++ CMS.Models/Article/ArticleDetailModel.cs
@@ -1,7 +1,17 @@
+using System;
+using CMS.Common.Enums;
+
 namespace CMS.Models.Article
 {
     public class ArticleDetailModel
     {
-        
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Url { get; set; }
+        public DateTime PublicationDateTime { get; set; }
+        public string Text { get; set; }
+        public bool Draft { get; set; }
+        public PageType PageType { get; set; }
     }
 }
---
+++ CMS.Web/Controllers/ArticleController.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using CMS.BL.Facades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CMS.Web.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly ArticleFacade _articleFacade;
+
+        public ArticleController(ArticleFacade articleFacade)
+        {
+            _articleFacade = articleFacade;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _articleFacade.GetAll());
+        }
+        
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var article = await _articleFacade.GetById(id.Value);
+            if (article == null)
+            {
+                return NotFound();
+            }
+
+            return View(article);
+        }
+    }
+}
